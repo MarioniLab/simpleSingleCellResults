21759cafba9b5974a8d2fde2f1fbb436cd78fdc1
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-09-08 18:24:48 +0100

    Stop rmarkdown following softlinks. Added instructions to compile the UMI workflow.

--- runner.R
+++ runner.R
@@ -1,1 +1,3 @@
-rmarkdown::render("work-1-reads.Rmd", clean=FALSE)
+rmarkdown::render("work-1-reads.Rmd", output_dir=".", intermediates_dir=".", clean=FALSE)
+rmarkdown::render("work-2-umis.Rmd", output_dir=".", intermediates_dir=".", clean=FALSE)
+
