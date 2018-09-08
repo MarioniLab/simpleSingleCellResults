8f09a6b72318a8f4f8c69f2cd075bee09021be35
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-09-08 21:01:27 +0100

    Added reference results for the MNN workflow.

--- runner.R
+++ runner.R
@@ -1,4 +1,5 @@
 rmarkdown::render("work-1-reads.Rmd", output_dir=".", intermediates_dir=".", clean=FALSE)
 rmarkdown::render("work-2-umis.Rmd", output_dir=".", intermediates_dir=".", clean=FALSE)
 rmarkdown::render("work-3-tenx.Rmd", output_dir=".", intermediates_dir=".", clean=FALSE)
+rmarkdown::render("work-5-mnn.Rmd", output_dir=".", intermediates_dir=".", clean=FALSE)
 
